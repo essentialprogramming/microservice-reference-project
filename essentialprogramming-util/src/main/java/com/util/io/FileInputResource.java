f71833f1afc10d369613c8c5f77368a1fff99899
Razvan Prichici <razvanpaulp@gmail.com>
2022-01-07 16:29:02 +0200

    Tightup implementation.

--- essentialprogramming-util/src/main/java/com/util/io/FileInputResource.java
+++ essentialprogramming-util/src/main/java/com/util/io/FileInputResource.java
@@ -21,17 +21,24 @@ public class FileInputResource implements InputResource, AutoCloseable {
     }
 
     public byte[] getBytes() throws IOException {
-        try (InputStream inputStream = getInputStream(); ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-            int nRead;
-            byte[] data = new byte[BYTE_RANGE];
-            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-
-            buffer.flush();
-            return buffer.toByteArray();
+        try (final InputStream inputStream = getInputStream()) {
+            return readStream(inputStream);
         }
+    }
 
+    /**
+     * Converts an input stream into a byte array.
+     */
+    public static byte[] readStream(final InputStream inputStream) throws IOException {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            final byte[] buffer = new byte[BYTE_RANGE];
+            int nRead;
+            while ((nRead = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                outputStream.write(buffer, 0, nRead);
+            }
+            outputStream.flush();
+            return outputStream.toByteArray();
+        }
     }
 
     public URL getFile() {
