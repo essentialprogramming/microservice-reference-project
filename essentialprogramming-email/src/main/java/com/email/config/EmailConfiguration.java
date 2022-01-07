c4170100ad87129b6023379465fc9a34d53b093a
markosk29 <markos.kosa29@gmail.com>
2022-01-07 14:30:47 +0200

    Remove invalid import.

--- essentialprogramming-email/src/main/java/com/email/config/EmailConfiguration.java
+++ essentialprogramming-email/src/main/java/com/email/config/EmailConfiguration.java
@@ -3,7 +3,6 @@ package com.email.config;
 import com.email.service.EmailService;
 import com.email.service.SendGridEmailService;
 
-import com.email.service.SendInBlueEmailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
