e614cbf69bff6114bfdf322d1a89e56ea980a4d7
Razvan Prichici <razvanpaulp@gmail.com>
2022-02-06 14:23:27 +0200

    Tightup implementation.

--- essentialprogramming-api/src/main/java/com/api/output/UserJSON.java
+++ essentialprogramming-api/src/main/java/com/api/output/UserJSON.java
@@ -11,6 +11,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class UserJSON implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String email;
     private String userKey;
     private String firstName;
